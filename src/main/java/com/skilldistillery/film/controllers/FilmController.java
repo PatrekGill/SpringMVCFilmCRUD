18c231b70226d989cb0e83af3b210883b5461778
Jack H <jack.himes3246@gmail.com>
2021-12-03 18:41:51 -0700

    added getFilmById to FilmController

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.skilldistillery.film.data.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
@@ -18,6 +20,16 @@ public class FilmController {
 		model.addAttribute("TEST", "Hello, Spring MVC");
 		
 		return "home";
+	}
+	@RequestMapping(path="GetFilmData.do", params="FilmId", method=RequestMethod.GET)
+	public String getFilmById(String filmId, Model model) {
+		int filmIdInt = Integer.parseInt(filmId);
+		Film f = filmDao.findFilmById(filmIdInt);
+		
+		model.addAttribute("film",f);
+		
+		
+		return "film";
 	}
 
 }
--- src/main/webapp/WEB-INF/home.jsp
+++ src/main/webapp/WEB-INF/home.jsp
@@ -8,6 +8,10 @@
 </head>
 <body>
 <h1>Films R Us!</h1>
+<h2>Search for a film:</h2>
+
+<h3>Search By Films ID</h3>
+<form action=""></form>
 
 ${TEST}
 
